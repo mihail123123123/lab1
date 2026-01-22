c36fb595a400fa40f74c5911346578eeee4e8a15
mihailgolubev <100130762+mihail123123123@users.noreply.github.com>
2026-01-22 11:24:37 +0300

    Update Form1.cs

--- LB1/Form1.cs
+++ LB1/Form1.cs
@@ -1,42 +1,113 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
-namespace WindowsFormsApp1
+namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        private List<Book> books = new List<Book>();
+        String[] ss = new String[100];
+
+
         public Form1()
         {
             InitializeComponent();
         }
-        public int oper;
-        public string value;
-        public void sum(string str)
+
+        // Добавление
+        private void button1_Click(object sender, EventArgs e)
         {
-            return;
+            listBox1.Items.Clear();
+            if (textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                books.Add(new Book(textBox2.Text, textBox3.Text, textBox4.Text));
+                foreach (var book in books)
+                {
+                    listBox1.Items.Add(book);
+                }
+                label4.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+            }
+            else
+            {
+                label4.Text = "Все поля дожны быть заполнены";
+            }
         }
-        public void subtraction(string str)
+        private void addListItem(string value)
         {
-            return;
+            this.listBox1.Items.Add(value);
         }
-        public void multiplication(string str)
+        // Удаление
+        private void button2_Click(object sender, EventArgs e)
         {
-            return;
+            var index = listBox1.SelectedIndex;
+            if (index == -1) return;
+            var name = books[index].Name;
+            books.RemoveAt(index);
+            MessageBox.Show(string.Format("Удалено {0}", name), "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            listBox1.Items.Clear();
+            foreach (var book in books)
+            {
+                listBox1.Items.Add(book);
+            }
         }
-        public void division(string str)
+
+        private void button3_Click(object sender, EventArgs e)
         {
-            return;
+            listBox1.Items.Clear();
+            foreach (var book in books)
+            {
+                listBox1.Items.Add(book);
+            }
         }
-        public string equal(string str)
+
+        private void Form1_Load(object sender, EventArgs e)
         {
-            return "";
+            listBox1.Items.Clear();
+            String path = @"J:\1.txt";
+            using (StreamReader sr = new StreamReader(path))
+                ss = File.ReadAllLines(path);
+            for (int i = 0; i < ss.Length; i++)
+            {
+                string[] temp = ss[i].Split(' ');
+                Book book = new Book(temp[0], temp[1], temp[2]);
+                books.Add(book);
+            }
+        }
+    }
+    public class Book
+    {
+        public Book() { }
+
+        public Book(
+            string author,
+            string name,
+            string publisher)
+        {
+            Author = author;
+            Name = name;
+            Publisher = publisher;
+        }
+
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Название: {0}, Автор: {1}, Издательство: {2}", Name, Author, Publisher);
         }
     }
 }
